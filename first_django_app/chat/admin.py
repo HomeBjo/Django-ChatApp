75689b947b2326aa305f427f96596fc0638875f9
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-04 11:42:04 +0200

    add

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Chat
 
 
 # Register your models here.    
 class MessageAdmin(admin.ModelAdmin):  #  admin modeladmin in der doc gefunden
-    fields = () # die felder in message drin
+    fields = [ 'chat','created_at','author', 'text' , 'receiver'] # die felder in message drin
     list_display = ( 'created_at','author', 'text' , 'receiver') # hier auf ebene messages 
     search_fields = ('text',) #inputfield mich such funktion 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -4,12 +4,14 @@ from django.db import models
 
 
 
-
+class Chat(models.Model):
+     created_at = models.DateField(default=date.today)
 
 # Create your models here.
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
     # chat = Chat Klasse Verknüpfen
+    chat =  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
