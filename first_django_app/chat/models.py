a41caf011c91f20c5171e6a5bed8821f46c718bd
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-04 12:52:59 +0200

    fix class chat

--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -12,6 +12,6 @@ class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
     # chat = Chat Klasse Verknüpfen
-    chat =  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+    chat =  models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from chat.models import Chat
+from chat.models import Message
+
 def index(request):
     if request.method == 'POST':
         print("Received data= " +  request.POST ['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST ['textmessage'],chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html')  #hier brauch man template pfad nicht angeben weil der da immmer von alleine rein guckt 
