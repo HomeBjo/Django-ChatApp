030266aeb9c02415c7ac7fa0e51879c8e67b52a6
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-05 10:48:30 +0200

    add redirect

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 from chat.models import Chat
 from chat.models import Message
 
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print("Received data= " +  request.POST ['textmessage'])
@@ -15,11 +17,12 @@ def index(request):
     return render(request, 'chat/index.html', {'messages': chatMessages})  #hier brauch man template pfad nicht angeben weil der da immmer von alleine rein guckt 
 
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username= request.POST.get('username'), password=request.POST.get('password')) 
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword' : True})
-    return render(request, 'auth/login.html')
+            return render(request, 'auth/login.html', {'wrongPassword' : True , 'redirect': redirect})
+    return render(request, 'auth/login.html',{'redirect': redirect})
