15e99b466755e9fd09a8c88418747c7c49ae714a
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-07 12:23:32 +0200

    add AJXX

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -9,11 +9,20 @@ from chat.models import Chat
 from chat.models import Message
 
 @login_required(login_url='/login/')
+
 def index(request):
     if request.method == 'POST':
         print("Received data= " +  request.POST ['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST ['textmessage'],chat=myChat, author=request.user, receiver=request.user)
+        message = Message.objects.create(text=request.POST ['textmessage'],chat=myChat, author=request.user, receiver=request.user)
+        
+        response_data = {
+            'created_at': message.created_at.strftime('[%B %d, %Y]'),  # Datum im gewünschten Format
+            'author': message.author.username,  # oder ein anderer relevanter Feldname
+            'text': message.text,
+        }
+        return JsonResponse(response_data)
+        
     chatMessages = Message.objects.filter(chat__id=1) # also hier gehen wir von message auf das objekt chat mit der id 1  was ein array ist    
     return render(request, 'chat/index.html', {'messages': chatMessages})  #hier brauch man template pfad nicht angeben weil der da immmer von alleine rein guckt 
 
